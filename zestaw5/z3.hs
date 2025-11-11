37c1639d6ce8521d2e038a6c1d71c572c292713b
karo <karolinykulas@gmail.com>
2025-11-11 22:55:53 +0100

    working z3

--- zestaw5/z3.hs
+++ zestaw5/z3.hs
@@ -1,7 +1,3 @@
 fib :: (Integer, Integer) -> [(Integer, Integer)]
 fib a = a : fib (snd a, fst a + snd a)
-
-fib2 :: [(Integer, Integer)] -> [Integer]
-fib2 = map snd (fib (0,1))
-
--- not working - to fix
+--  map fst $ fib (0, 1)
