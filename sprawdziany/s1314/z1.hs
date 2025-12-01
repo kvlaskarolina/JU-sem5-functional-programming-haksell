bf13b902dccaff9acdbe542538b91bd0fb9b75e2
karo <karolinykulas@gmail.com>
2025-12-01 17:22:00 +0100

    tasks added

---
+++ sprawdziany/s1314/z1.hs
@@ -0,0 +1,12 @@
+--Zadanie 1. Napisa´c funkcj˛e oddbins n, generuj ˛ac ˛a list˛e wszystkich ci ˛agów 
+--binarnych o długo´sci n, w których liczba jedynek jest nieparzysta. Ci ˛agi reprezentujemy
+--w postaci list, zatem np.
+--oddbins 3 = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 1]].
+--Kolejno´s´c ci ˛agów w li´scie nie ma znaczenia. Zakładamy, ˙ze n ­ 1.
+
+oddbins :: Int -> [[Int]]
+oddbins = 
+
+p :: Int -> [Int] -> [Int]
+p 
+
