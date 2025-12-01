e240587f869b22e4c8bf02a091eb5a6a3dc6e55f
karo <karolinykulas@gmail.com>
2025-12-01 15:15:31 +0100

    z7 done - first set fully done :DD

--- zestaw1/z7.hs
+++ zestaw1/z7.hs
@@ -1,5 +1,10 @@
 czyDoskonala :: Int -> Bool
-czyDoskonala n = n == sum (dzielniki n)
+czyDoskonala n = (n == sum (dzielniki (n,[1..n])))
+
+czyDzieli :: Int -> Int -> [Int]
+czyDzieli  a b | a `mod` b == 0 = [b]
+               | otherwise = []
 
 dzielniki :: (Int, [Int]) -> [Int]
-dzielniki p = fst
+dzielniki x | (fst x) == (head (snd x)) = []
+            | otherwise = (czyDzieli  (fst x) (head (snd x))) ++ dzielniki ((fst x), tail (snd x)) 
